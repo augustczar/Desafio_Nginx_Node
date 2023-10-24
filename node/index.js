a3ce9ffa74bbf8a1d183a1592a95ae29cba5029e
augustczar <augustczarramos@gmail.com>
2023-10-24 09:14:29 -0300

    add wait-for

--- node/index.js
+++ node/index.js
@@ -10,12 +10,23 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Augusto')`
-connection.query(sql)
+var lista = "";
+const sqlCreateTable = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))`
+connection.query(sqlCreateTable)
+
+const sqlInsertTable = `INSERT INTO people(name) values('Augusto')`
+connection.query(sqlInsertTable)
+
+const sqlSelectTable = `SELECT name FROM people`
+connection.query(sqlSelectTable, (err, results, fields) => {
+    if (err) throw err
+    lista = JSON.stringify(results)
+})
+
 connection.end()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    res.send('<h1>Full Cycle Rocks!</h1>' + '</h2> NameList: </h2>' + lista)
 })
 
 app.listen(port, ()=> {
